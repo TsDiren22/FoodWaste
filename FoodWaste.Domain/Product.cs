e502354bbcf73acfd6724bebee4181244d45decd
Diren Ozturk <od.ozturk@student.avans.nl>
2023-10-26 00:46:10 +0200

    FixedViewModel

--- FoodWaste.Domain/Product.cs
+++ FoodWaste.Domain/Product.cs
@@ -14,8 +14,6 @@ namespace FoodWaste.Domain
         public bool IsAlcoholic { get; set; }
         public byte[] Picture { get; set; }
         public string PictureType { get; set; }
-        [NotMapped] // Use this attribute if you don't want to map it to the database
-        public IFormFile? PictureFile { get; set; }
         public List<Package> Packages { get; set; } = new();
     }
 }
